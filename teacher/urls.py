023dc6f54ce2ccbb60b596c8fe7123b31c00666e
Vedant Jain <31737729+mr-malware0x01@users.noreply.github.com>
2021-05-19 12:20:09 +0530

    Update urls.py

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from teacher import views
+from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.conf.urls.static import static
 from django.conf import settings
